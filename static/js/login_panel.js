634b8de82cfe3dafae7d8d41868f4022d6b26bc8
1cetsw <1cetsw@gmail.com>
2022-10-26 19:12:08 +0200

    add login/register system and bootsstrap style

--- static/js/login_panel.js
+++ static/js/login_panel.js
@@ -1,12 +1,19 @@
-   var modal = document.getElementById('id01');
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+let modal = document.getElementById('id01');
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-    var modal = document.getElementById('id02');
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+}
+let modal = document.getElementById('id02');
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
+}
+
+let modal = document.getElementById('id03');
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
