15cf4c9bf919e5ee4dff65065ebde666acc97eb8
Al3ksanderD <olo1045@wp.pl>
2022-10-28 13:22:09 +0200

    Update board.js

--- static/js/board.js
+++ static/js/board.js
@@ -1,9 +1,10 @@
 let todoId = 0
 let draggables = document.querySelectorAll('.draggable')
 let containers = document.querySelectorAll('.task-col')
+let deleteButtons = document.querySelectorAll("delTodo")
 
-console.log(draggables)
 document.getElementById("task-add-button").addEventListener("click", addTask);
+
 
 function addTask() {
     let title = document.getElementById("task-title").value
@@ -11,27 +12,64 @@ function addTask() {
 
     renderTask(title,priority)
     updateVars()
-    console.log(draggables)
+}
+
+function delTask(event) {
+    console.log(event.target.dataset.Del, " is going to get deleted");
+
 }
 
 function updateVars() {
     draggables = document.querySelectorAll('.draggable');
     containers = document.querySelectorAll('.task-col');
+    deleteButtons = document.querySelectorAll(".delete-task-button")
     checkEvents()
 }
 
 function renderTask(title, priority) {
-    console.log("render")
+    console.log("render bla bla")
+
+    const edit = document.createElement('button');
+    edit.innerText = "Edit Todo";
+    edit.id = `editTodo`;
+    edit.dataset.Edit = `todo${todoId}`
+    edit.classList.add("btn");
+    edit.classList.add("btn-secondary");
+    edit.classList.add("edit-task-button");
+
+    const button = document.createElement('button');
+    button.innerText = "Delete Todo";
+    button.id = `delTodo`;
+    button.dataset.Del = `todo${todoId}`
+    button.classList.add("btn");
+    button.classList.add("btn-secondary");
+    button.classList.add("delete-task-button");
+
     const todo = document.createElement('div');
     todo.setAttribute("draggable", true);
-    todo.innerText = title;
+
+    todo.innerHTML = title + "<br>";
     todo.id = `todo${todoId}`;
     todo.classList.add(`todo${priority}`);
     todo.classList.add("draggable");
+
+
+
     document.getElementById("task-to-do-container").appendChild(todo);
+    document.getElementById(`todo${todoId}`).appendChild(button);
+    document.getElementById(`todo${todoId}`).appendChild(edit);
     todoId += 1;
 }
 function checkEvents() {
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", () => {
+             console.log(event.target.dataset.Del + " is going to get deleted");
+             let idToDel = event.target.dataset.Del;
+             let element = document.getElementById(idToDel);
+             element.parentNode.removeChild(element);
+
+        });
+    })
     draggables.forEach(draggable => {
         draggable.addEventListener('dragstart', () => {
             console.log("dragstart")
@@ -57,3 +95,8 @@ function checkEvents() {
         })
     })
 }
+
+
+ // function refresh(){
+ //        window.location.reload("Refresh")
+ //      }
