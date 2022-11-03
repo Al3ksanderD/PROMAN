260aab60e284c628a1fcb5802eb7ab2accea242e
Al3ksanderD <olo1045@wp.pl>
2022-11-03 13:47:29 +0100

    New stuff: - renaming columns - deleting columns - adding new columns - a little refactor

--- static/js/board.js
+++ static/js/board.js
@@ -1,11 +1,55 @@
 let todoId = 0
+let columnId = 4
 let draggables = document.querySelectorAll('.draggable')
 let containers = document.querySelectorAll('.task-col')
 let deleteButtons = document.querySelectorAll("delTodo")
-let myEditableElement = document.querySelectorAll(".edit-task-button");
+let myEditableElement = document.querySelectorAll(".edit-task-button")
+let deleteColumns = document.querySelectorAll(".delete-col-button")
+let editColumns = document.querySelectorAll(".edit-col-button")
+
 
 document.getElementById("task-add-button").addEventListener("click", addTask);
+document.getElementById("column-add-button").addEventListener("click", addColumn);
 
+updateVars();
+
+
+
+
+function addColumn() {
+
+    const newColumn = document.createElement('div');
+    newColumn.classList.add("col")
+    newColumn.classList.add("task-col")
+    newColumn.id = `task-col-${columnId}`
+
+    const title = document.createElement('div');
+    title.classList.add("col-title")
+    title.id = `col-title-${columnId}`
+
+    const titleText = document.createElement('div');
+    titleText.classList.add("title")
+    titleText.innerText = "Unnamed"
+
+    const deleteButton = createDeleteColButton();
+    const editButton = createEditColButton();
+
+    const todo_container = document.createElement('div');
+
+    document.getElementById("col-container").appendChild(newColumn);
+
+    document.getElementById(`task-col-${columnId}`).appendChild(title);
+    document.getElementById(`col-title-${columnId}`).appendChild(titleText);
+    document.getElementById(`col-title-${columnId}`).appendChild(deleteButton);
+    document.getElementById(`col-title-${columnId}`).appendChild(editButton);
+
+    document.getElementById(`task-col-${columnId}`).appendChild(todo_container);
+
+    columnId += 1;
+    updateVars();
+
+
+}
 
 function addTask() {
     let title = document.getElementById("task-title").value
@@ -13,7 +57,24 @@ function addTask() {
 
     renderTask(title, priority)
     updateVars()
-};
+}
+
+function editCol(event) {
+    console.log("editcol function")
+    let editedValue = document.getElementById("afterColEdit").value
+    let newValue = document.getElementById(event.currentTarget.myParam)
+    let del = createDeleteColButton();
+    let edit = createEditColButton();
+
+
+    newValue.innerHTML = '<div class="title">' + editedValue + '</div>';
+
+    newValue.appendChild(del);
+    newValue.appendChild(edit);
+
+    updateVars()
+
+}
 
 function editTask(event) {
     let string = event.currentTarget.myParam
@@ -38,11 +99,40 @@ function editTask(event) {
 
 }
 
+function delCol() {
+    console.log(event.target);
+    let child = event.target.parentNode
+    child.parentNode.remove();
+
+    updateVars()
+}
+
 function delTask(element) {
 
     element.parentNode.removeChild(element);
 
 }
+function createDeleteColButton() {
+    const button = document.createElement('button');
+    button.innerText = "Delete";
+    button.id = `deleteColButton`;
+    button.classList.add("btn");
+    button.classList.add("btn-secondary");
+    button.classList.add("delete-col-button");
+    return button;
+}
+
+function createEditColButton() {
+    const edit = document.createElement('button');
+    edit.innerText = "Edit";
+    edit.id = `editColButton`;
+    edit.classList.add("btn");
+    edit.classList.add("btn-secondary");
+    edit.classList.add("edit-col-button");
+    return edit;
+}
+
+
 
 function createDeleteButton(id) {
     const button = document.createElement('button');
@@ -73,6 +163,8 @@ function updateVars() {
     containers = document.querySelectorAll('.task-col');
     deleteButtons = document.querySelectorAll(".delete-task-button");
     myEditableElement = document.querySelectorAll(".edit-task-button");
+    deleteColumns = document.querySelectorAll(".delete-col-button")
+    editColumns = document.querySelectorAll(".edit-col-button")
 
     checkEvents()
 }
@@ -94,13 +186,41 @@ function renderTask(title, priority) {
     todo.classList.add("draggable");
 
 
-    document.getElementById("task-to-do-container").appendChild(todo);
+    document.getElementById("task-col-1").appendChild(todo);
     document.getElementById(`todo${todoId}`).appendChild(button);
     document.getElementById(`todo${todoId}`).appendChild(edit);
     todoId += 1;
 }
 
 function checkEvents() {
+
+    deleteColumns.forEach(column => {
+        column.addEventListener("click", delCol)
+    })
+
+
+    editColumns.forEach(column => {
+    column.addEventListener("click", () => {
+        let parentId = event.target.parentNode.id;
+        let parent = event.target.parentNode
+        // let targetChild = document.getElementById(parentId).getElementsByClassName("title")[0];
+        let toChange = parent
+        let inputArea = '<textarea id="afterColEdit"> Edit Col Name </textarea>'
+        let buttonArea = '<button id="afterColEditButton"> Edit! </button>'
+        toChange.innerHTML = inputArea + "<br>" + buttonArea;
+        let afterEdit = document.getElementById("afterColEditButton")
+        afterEdit.myParam = parentId
+        afterEdit.addEventListener("click", editCol);
+
+
+
+
+
+        // let inputArea = '<textarea id="afterEdit"> </textarea>'
+        // let buttonArea = '<button id="afterEditButton"> Edit! </button>'
+        // toChange.innerHTML = inputArea + "<br>" + buttonArea;
+    })
+})
 
     myEditableElement.forEach(button => {
         button.addEventListener("click", () => {
